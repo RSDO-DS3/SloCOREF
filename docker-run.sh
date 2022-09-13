77ba52776924d36edc739d2e24e1150589068f2b
Slavko Zitnik <slavko@zitnik.si>
2022-09-13 04:11:23 +0200

    port update

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-docker run --rm -it --name slo-coref \
-  -p 5020:5020 \
+docker run \
+  -d \
+  -it \
+  --name slo-coref \
+  -p 5002:5020 \
+  --restart always \
   --env COREF_MODEL_PATH="/app/data/bert_based/" \
   --mount type=bind,source="`pwd`/model/cseb_senticoref/",destination="/app/data/bert_based/",ro \
   slo-coref
